ca7153759ffe0b47bd8ba53b6c35916be7cf54c2
yanchuan <yanchuan1990@users.noreply.github.com>
2016-12-14 11:14:43 +0800

    Update cardList.js
    update the function order.

--- js/cardList.js
+++ js/cardList.js
@@ -1,28 +1,58 @@
-
-	/**
-	 * 字段含义
-	 * "bankNumber": "银行卡号",
-		"bankName": "银行名称",
-		"bankCount": "卡片金额",
-		"bankStartDate": 当月账单日,
-		"bankEndDate": 账单后最后还款日,
-		"differ":卡片可免息天数
-	 */
-
-	//每个月的账单日 最后还款日设置
-	function newDate(m,day){
-		var date0 = new Date();
-		var newDate = dateFormat(new Date(date0.getFullYear(), date0.getMonth() + m, day));
-		
-		return newDate;
+var dataList = [{
+	"bankNumber": "123456",
+	"bankName": "招商银行",
+	"bankCount": "2000",
+	"bankStartDate": [0, 6],
+	"bankEndDate": [0, 24],
+	"differ": 48
+}, {
+	"bankNumber": "58585",
+	"bankName": "民生银行",
+	"bankCount": "2500",
+	"bankStartDate": [0, 23],
+	"bankEndDate": [1, 13],
+	"differ": 51
+}, {
+	"bankNumber": "58585",
+	"bankName": "广发银行",
+	"bankCount": "2500",
+	"bankStartDate": [0, 2],
+	"bankEndDate": [0, 22],
+	"differ": 51
+}, {
+	"bankNumber": "58585",
+	"bankName": "交通银行",
+	"bankCount": "2500",
+	"bankStartDate": [0, 14],
+	"bankEndDate": [1, 8],
+	"differ": 55
+}];
+//日期格式化
+function dateFormat(da, isLong) {
+	var dte;
+	if(typeof(da) == "string") {
+		dte = new Date(da.replace(/-/g, "/"));
+	} else {
+		dte = da;
 	}
 
-	//格式化日期
-	function FormatDate(strTime) {
-		var date1 = new Date(strTime);
-		return date1.getFullYear() + "-" + (date1.getMonth() + 1) + "-" + date1.getDate();
+	var dateOBj = {
+		"day": dte.getDate(),
+		"month": isLong ? dte.getMonth() + 2 : dte.getMonth() + 1,
+		"year": dte.getFullYear()
 	}
-	//计算日期 差 刷卡日 减去 账单日 = date2 - date1
+	dateOBj.ymd = dateOBj.year + "-" + dateOBj.month + "-" + dateOBj.day;
+
+	return dateOBj;
+};
+//处理初始数据时 当日日期函数 
+function newDate(m, day) {
+	var date = new Date();
+	var newDate = dateFormat(new Date(date.getFullYear(), date.getMonth() + m, day));
+
+	return newDate;
+}
+//计算日期 差 刷卡日 减去 账单日 = date2 - date1
 	function calb(date1, date2) {
 
 		var y1 = date1.year,
@@ -39,25 +69,8 @@
 
 		return dateCount;
 	}
-	//日期格式化
-	function dateFormat(da, isLong) {
-		var dte;
-		if(typeof(da) == "string") {
-			dte = new Date(da.replace(/-/g, "/"));
-		} else {
-			dte = da;
-		}
-
-		var dateOBj = {
-			"day": dte.getDate(),
-			"month": isLong ? dte.getMonth() + 2 : dte.getMonth() + 1,
-			"year": dte.getFullYear()
-		}
-		dateOBj.ymd = dateOBj.year + "-" + dateOBj.month + "-" + dateOBj.day;
-
-		return dateOBj;
-	};
-	/*免息期计算   free(today,days,stday,obj.differ);
+	/*免息期计算   free(days,stday,obj.differ);
+	 * today ： 距离本月账单日具体天数
 	 * days : 当前日期与账单日 相差天数  正值：账单日之前 负值：账单日之后
 	 * stday 账单日
 	 * eday 当月还款日
@@ -83,94 +96,56 @@
 		}
 		return lastday;
 	}
-	//根据天数 计算日期 dt当前日期 daynum距离天数
-	function cala(dt, dayNum) {
-		let y = dt.year,
-			m = dt.month,
-			d = dt.day,
-			ddd = dayNum;
+//处理数据渲染函数
+function cardlist(dataList) {
 
-		ttt = new Date(y, m - 1, d).getTime() + ddd * 24000 * 3600;
+	var today = dateFormat(new Date());
+	
+	var obj = dataList;
+	
+	var html = "";
+	for(var i = 0; i < dataList.length; i++) {
+		//账单日
+		var stday = obj[i].bankStartDate;
+		var eday = obj[i].bankEndDate;
+		//相差天数 = 账单日 - 当前日期
+		var days = calb(today, stday);
+//		alert(days);
+		var lastday = free(today,days, stday, eday, obj[i].differ);
+		//免息天数计算
+		var xxx = calb(stday, eday);
 
-		theday = new Date();
-		theday.setTime(ttt);
+		html += '<div class="cardList">' +
+			'<div class="cardListchild">' +
+			'<p class="cardListTitle">' + obj[i].bankName + '</p>' +
+			'<ul class="mui-table-view">' +
+			'<li class="mui-table-view-cell">今日刷卡可免息使用<label class="ftbd">' + lastday.differ + '</label>天</li>' +
+			'<li class="mui-table-view-cell">今日距离账单日<label class="ftbd">' + days + '</label>天</li>' +
+			'<li class="mui-table-view-cell">信用额度：<label class="edu">' + obj[i].bankCount + '</label></li>' +
+			'<li class="mui-table-view-cell">本月账单日:' + stday.ymd + '</li>' +
+			'<li class="mui-table-view-cell">最后还款日:' + eday.ymd + '</li>' +
+			'<li class="mui-table-view-cell">今日消费最后还款日:' + lastday.ymd + '</li>' +
+			'<li class="mui-table-view-cell"><p>今日消费账单区间:</p><p>' + lastday.interval + '</p></li>' +
+			'</ul>' +
 
-		return theday;
+			'</div>' +
+			'</div>'
 	}
-	function cardlist(dataList) {
-			//当日日期
-			var today = dateFormat(new Date());
+	//cardlist 页面渲染
+	document.getElementById("cardlist").innerHTML = html;
+}
 
-			var obj = dataList;
-			var html = "";
-			for(var i = 0; i < dataList.length; i++) {
-				//账单日
-				var stday = obj[i].bankStartDate;
-				var eday = obj[i].bankEndDate;
-				//相差天数 = 账单日 - 当前日期
-				var days = calb(today, stday);
-				
-				var lastday = free(today,days, stday, eday, obj[i].differ);
-				//免息天数计算
-				var xxx = calb(stday, eday);
-	
-				html += '<div class="cardList">' +
-					'<div class="cardListchild">' +
-					'<p class="cardListTitle">' + obj[i].bankName + '</p>' +
-					'<ul class="mui-table-view">' +
-					'<li class="mui-table-view-cell">今日刷卡可免息使用<label class="ftbd">' + lastday.differ + '</label>天</li>' +
-					'<li class="mui-table-view-cell">今日距离账单日<label class="ftbd">' + days + '</label>天</li>' +
-					'<li class="mui-table-view-cell">信用额度：<label class="edu">' + obj[i].bankCount + '</label></li>' +
-					'<li class="mui-table-view-cell">本月账单日:' + stday.ymd + '</li>' +
-					'<li class="mui-table-view-cell">最后还款日:' + eday.ymd + '</li>' +
-					'<li class="mui-table-view-cell">今日消费最后还款日:' + lastday.ymd + '</li>' +
-					'<li class="mui-table-view-cell"><p>今日消费账单区间:</p><p>' + lastday.interval + '</p></li>' +
-					'</ul>' +
-	
-					'</div>' +
-					'</div>'
-			}
-			//cardlist 页面渲染
-			// $("#cardlist").append(html);
-			document.getElementById("cardlist").innerHTML = html;
-	}
-	function init() {
+function init() {
 
-		var dataList = [{
-			"bankNumber": "123456",
-			"bankName": "招商银行",
-			"bankCount": "2000",
-			"bankStartDate": newDate(0,6),
-			"bankEndDate": newDate(0,24),
-			"differ": 48
-		}, {
-			"bankNumber": "58585",
-			"bankName": "民生银行",
-			"bankCount": "2500",
-			"bankStartDate": newDate(0,23),
-			"bankEndDate": newDate(1,13),
-			"differ": 51
-		}, {
-			"bankNumber": "58585",
-			"bankName": "广发银行",
-			"bankCount": "2500",
-			"bankStartDate": newDate(0,2),
-			"bankEndDate": newDate(0,22),
-			"differ": 51
-		}, {
-			"bankNumber": "58585",
-			"bankName": "交通银行",
-			"bankCount": "2500",
-			"bankStartDate": newDate(0,14),
-			"bankEndDate": newDate(1,8),
-			"differ": 55
-		}];
-		
-		cardlist(dataList);
+	for(var i = 0; i < dataList.length; i++) {
+
+		dataList[i].bankStartDate = newDate(dataList[i].bankStartDate[0], dataList[i].bankStartDate[1]);
+		dataList[i].bankEndDate = newDate(dataList[i].bankEndDate[0], dataList[i].bankEndDate[1]);
 	}
-	
-	window.onload = init();
-	
-	document.getElementById("info").addEventListener("click",function(){
-		window.location.href = "template/info.html";
-	});
+
+	cardlist(dataList);
+}
+document.getElementById("info").addEventListener("click",function(){
+	window.location.href = "template/info.html";
+});
+window.onload = init();
